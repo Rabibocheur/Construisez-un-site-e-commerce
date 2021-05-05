67888e923eb087d382c279ad6abdb54a521df14b
Rabibocheur <beaupuyg@outlook.fr>
2021-05-05 18:09:41 +0200

    refonte de la creation du DOM

--- getItem.js
+++ getItem.js
@@ -1,8 +1,26 @@
 async function getItem() {
   let getUrl = window.location.search;
   const searchParams = new URLSearchParams(getUrl);
-
   let response = await getProducts(searchParams.get("id"));
-  document.getElementById("img").src = response.imageUrl;
+  const item = document.getElementById("item");
+  let colors = "";
+  for (let i = 0; i < response.colors.length; i++) {
+    colors += `<div class="" style="background-color: ${response.colors[i]};">f</div>`;
+  }
+  item.innerHTML = `
+  <img class="col-6 img-thumbnail" src="${response.imageUrl}" alt="" />
+  <section class="col-md-6">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-body">
+      <h1>${response.name}</h1>
+        <h4>Prix : ${response.price}</h4>
+        <p>${response.description}</p>
+        <div class="d-flex">
+          ${colors}
+        </div>
+      </div>
+    </div>
+  </section>
+  `;
 }
 getItem();
--- getItems.js
+++ getItems.js
@@ -1,44 +1,31 @@
 /*
  * Affichage des produits
  */
-getItems();
 async function getItems() {
   let response = await getProducts();
   const allItems = document.getElementById("items");
 
   for (let i = 0; i < response.length; i++) {
-    // Créations des cartes
-    let sectionItem = document.createElement("section");
-    let cardItem = document.createElement("div");
-    let imgItem = document.createElement("img");
-    let cardBodyItem = document.createElement("div");
-    let nameItem = document.createElement("p");
-    let btnItem = document.createElement("button");
+    allItems.innerHTML += `
+    <section class="col-md-4 col-sm-6">
+      <div class="card mb-4 shadow-sm">
+        <img class="img-responsive" style="height: 250px" src="${response[i].imageUrl}">
+        <div class="card-body">
+        <p>${response[i].name}</p>
+          <button id="${response[i]._id}" class="btn btn-sm btn-outline-secondary">
+          Plus d'infos
+          </button>
+        </div>
+      </div>
+    </section>
+    `;
+  }
 
-    // Créations des classes bootstrap
-    sectionItem.setAttribute("class", "col-md-4 col-sm-6");
-    cardItem.setAttribute("class", "card mb-4 shadow-sm");
-    imgItem.setAttribute("class", "img-card card-img-top");
-    imgItem.setAttribute("src", response[i].imageUrl);
-    cardBodyItem.setAttribute("class", "card-body");
-    btnItem.setAttribute("class", "btn btn-sm btn-outline-secondary");
-
-    nameItem.textContent = response[i].name;
-    btnItem.id = response[i]._id;
-    btnItem.textContent = "Plus d'infos";
-
-    // Affichage des cartes
-    allItems.appendChild(sectionItem);
-    sectionItem.appendChild(cardItem);
-    cardItem.appendChild(imgItem);
-    cardItem.appendChild(cardBodyItem);
-    cardBodyItem.appendChild(nameItem);
-    cardBodyItem.appendChild(btnItem);
-
-    // Evenement click de redirection sur la page item
-    let btn = document.getElementById(response[i]._id);
-    btn.addEventListener("click", () => {
-      window.location.href = "item.html?id=" + btn.id;
+  let btn = document.querySelectorAll("button");
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      window.location.href = "item.html?id=" + response[i]._id;
     });
   }
 }
+getItems();
